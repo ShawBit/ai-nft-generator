b9363c3549f19d9ff88ce873da49861d27ca9406
ShawBit <shawbitx@gmail.com>
2024-04-02 17:22:54 +0800

    feat: upload image to nft.storage

--- src/components/MintForm.tsx
+++ src/components/MintForm.tsx
@@ -1,21 +1,25 @@
 'use client';
 import Image from 'next/image';
+import { Blob, NFTStorage } from 'nft.storage';
 import { FormEvent, useState } from 'react';
 
 export function MintForm() {
   const [name, setName] = useState('');
   const [description, setDescription] = useState('');
   const [image, setImage] = useState('');
+  const [nftUrl, setNFTUrl] = useState('');
 
   const submitHandler = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    createImage();
+    const image = await createImage();
+    uploadImage(image);
   };
 
-  const url = 'https://api-inference.huggingface.co/models/stabilityai/stable-diffusion-2-1';
+  const modelUrl = 'https://api-inference.huggingface.co/models/stabilityai/stable-diffusion-2-1';
+
   const createImage = async () => {
     console.log('Generated Iamge...');
-    const response = await fetch(url, {
+    const response = await fetch(modelUrl, {
       method: 'POST',
       headers: { Authorization: `Bearer ${process.env.NEXT_PUBLIC_HUGGING_FACE_API_KEY}` },
       body: JSON.stringify({ inputs: description }),
@@ -25,6 +29,26 @@ export function MintForm() {
     const blobUrl = URL.createObjectURL(result);
     setImage(blobUrl);
     return result;
+  };
+
+  const uploadImage = async (image: Blob) => {
+    console.log('Uploading image...');
+    // Create instance to NFT.Storage
+    const nftStorage = new NFTStorage({ token: `${process.env.NEXT_PUBLIC_NFT_STORAGE_API_KEY}` });
+
+    const { ipnft } = await nftStorage.store({
+      image: image,
+      name: name,
+      description: description,
+    });
+    // const ipnft = await nftStorage.storeBlob(image);
+
+    const url = `https://ipfs.io/ipfs/${ipnft}/metadata.json`;
+    console.log('url: ', url);
+
+    setNFTUrl(url);
+
+    return url;
   };
 
   return (
@@ -59,7 +83,7 @@ export function MintForm() {
       </div>
       <p className="flex justify-center items-center">
         View&nbsp;
-        <a href="" target="_blank" rel="noreferrer">
+        <a href={nftUrl} target="_blank" rel="noreferrer">
           Metadata
         </a>
       </p>
